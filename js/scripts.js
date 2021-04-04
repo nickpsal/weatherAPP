20d618f1aeb17f3087b00acbca311d0c0a4c86b4
NIKOLAOS PSALTAKIS <nickpsal@gmail.com>
2021-04-04 14:19:57 +0300

    commit 04/04/2021 2

--- js/scripts.js
+++ js/scripts.js
@@ -82,18 +82,18 @@ function getData(data) {
     let todayIcon = data.current.weather[0].icon;
     let tommorowIcon = data.daily[0].weather[0].icon;
     let tdaIcon = data.daily[1].weather[0].icon;
+    let time1Icon = data.hourly[1].weather[0].icon;
+    let time2Icon = data.hourly[2].weather[0].icon;
+    let time3Icon = data.hourly[3].weather[0].icon;
+    let time4Icon = data.hourly[4].weather[0].icon;
+    let time5Icon = data.hourly[5].weather[0].icon;
     let FulIconToday = iconBaseURl + todayIcon + iconformat;
     let FulIconTommoerow = iconBaseURl + tommorowIcon + iconformat;
     let FulIconTda = iconBaseURl + tdaIcon + iconformat;
-    let time1Icon = data.hourly[1].weather[0].icon;
     let FullTime1Icon = iconBaseURl + time1Icon + iconformat;
-    let time2Icon = data.hourly[2].weather[0].icon;
     let FullTime2Icon = iconBaseURl + time2Icon + iconformat;
-    let time3Icon = data.hourly[3].weather[0].icon;
     let FullTime3Icon = iconBaseURl + time3Icon + iconformat;
-    let time4Icon = data.hourly[4].weather[0].icon;
     let FullTime4Icon = iconBaseURl + time4Icon + iconformat;
-    let time5Icon = data.hourly[5].weather[0].icon;
     let FullTime5Icon = iconBaseURl + time5Icon + iconformat;
 
     //print data
